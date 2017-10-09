eb51f471043a324afcb658050dbe2e6b89e38513
Versionnnn <mail4gaozeyao@gmail.com>
2017-10-09 17:01:19 +1100

    first commit

---
+++ SensorReceiver/LineChart.swift
@@ -0,0 +1,21 @@
+//
+//  LineChart.swift
+//  SensorReceiver
+//
+//  Created by aaron on 19/09/2017.
+//  Copyright © 2017 aaron. All rights reserved.
+//
+
+import UIKit
+
+class LineChart: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
